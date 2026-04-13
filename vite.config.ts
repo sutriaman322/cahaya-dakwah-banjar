bafc16b5997a7e8bfdd5bc122ff21c7ac4469311
sutriaman <75231558+sutriaman322@users.noreply.github.com>
2026-04-13 11:44:06 +0700

    Update Vite configuration with new plugins and base

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/cahaya-dakwah-banjar/', // ⬅️ ini penting banget
+})
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
